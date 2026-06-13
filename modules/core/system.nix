89253f4c644b9538d8170a64aef06ea38fa117a5
ChunChoc <chunchoc@protonmail.com>
2026-06-13 15:53:27 -0600

    Mullvad se abre en pantalla completa y ahora se ignora el DNS de el DHCP

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -6,12 +6,12 @@
   # Zona horaria y Locale
   time.timeZone = "America/Guatemala";
   i18n.defaultLocale = "en_US.UTF-8";
-  
+
   # Teclado latino en TTY
   console = {
      keyMap = "la-latin1";
   };
-  
+
   # Teclado latino en el sistema
   services.xserver.xkb = {
     layout = "latam";
@@ -20,7 +20,7 @@
 
   # Habilitar Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  
+
   # Limpieza automática de basura
   nix.gc = {
     automatic = true;
@@ -30,8 +30,9 @@
   # Desduplicador automático
   nix.settings.auto-optimise-store = true;
 
-  # DNS de Cloudflare y AdGuard (Public Default)
-  networking.nameservers = [ "1.1.1.1" "94.140.14.14" ];
+  # DNS de Quad9, Cloudflare y AdGuard
+  networking.networkmanager.dns = "none";
+  networking.nameservers = [ "9.9.9.9" "94.140.14.14" "1.1.1.1" ];
 
   # Gestor de autorización a nivel de sistema (Para run0)
   security.polkit.enable = true;
