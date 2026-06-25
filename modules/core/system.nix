db1dfc622ff3d815483637aeeb6d7b70434162c6
ChunChoc <chunchoc@protonmail.com>
2026-06-24 20:09:43 -0600

    verificación de claude

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -83,7 +83,7 @@
             "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
             "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
           ];
-          cache_file = "public-resolvers.md";
+          cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
           minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
         };
         relays = {
@@ -91,7 +91,7 @@
             "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/relays.md"
             "https://download.dnscrypt.info/resolvers-list/v3/relays.md"
           ];
-          cache_file = "relays.md";
+          cache_file = "/var/cache/dnscrypt-proxy/relays.md";
           minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
         };
       };
