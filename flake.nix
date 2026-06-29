9a3bd4a6e758cd5a4bced4354ecbcdc99eb41081
ChunChoc <chunchoc@protonmail.com>
2026-06-29 17:34:21 -0600

    se cambiaron las animaciones tipo pixel

--- flake.nix
+++ flake.nix
@@ -7,73 +7,81 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
-  let
-    # Función helper para crear configuraciones de host
-    mkHost = { hostname, monitorSettings }: nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    let
+      # Función helper para crear configuraciones de host
+      mkHost =
+        { hostname, monitorSettings }:
+        nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
 
-      specialArgs = { inherit inputs monitorSettings; };
+          specialArgs = { inherit inputs monitorSettings; };
 
-      modules = [
-        ./hosts/${hostname}/default.nix
+          modules = [
+            ./hosts/${hostname}/default.nix
 
-        # Módulos comunes a todos los hosts
-        ./lib/options.nix
-        ./modules/core
-        ./modules/features
-        ./modules/desktop.nix
+            # Módulos comunes a todos los hosts
+            ./lib/options.nix
+            ./modules/core
+            ./modules/features
+            ./modules/desktop.nix
 
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.backupFileExtension = "backup";
-          home-manager.extraSpecialArgs = { inherit inputs monitorSettings; };
-          home-manager.users.chunchoc = import ./modules/home/default.nix;
-        }
-      ];
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
+              home-manager.extraSpecialArgs = { inherit inputs monitorSettings; };
+              home-manager.users.chunchoc = import ./modules/home/default.nix;
+            }
+          ];
+        };
+    in
+    {
+      nixosConfigurations = {
+
+        # VM de desarrollo
+        nixos-vm = mkHost {
+          hostname = "vm";
+          monitorSettings = {
+            name = "Virtual-1";
+            width = 1920;
+            height = 1080;
+            refreshRate = 60;
+            scale = 1.0;
+          };
+        };
+
+        # Thinkpad - trabajo y programación
+        thinkpad = mkHost {
+          hostname = "thinkpad";
+          monitorSettings = {
+            name = "eDP-1"; # Cambiar según tu monitor
+            width = 1920;
+            height = 1080;
+            refreshRate = 60;
+            scale = 1.0;
+          };
+        };
+
+        # Aorus - gaming y desarrollo ocasional
+        aorus = mkHost {
+          hostname = "aorus";
+          monitorSettings = {
+            name = "eDP-1"; # Cambiar según tu monitor
+            width = 1920;
+            height = 1080;
+            refreshRate = 144; # 144Hz probablemente
+            scale = 1.0;
+          };
+        };
+
+      };
     };
-  in
-  {
-    nixosConfigurations = {
-
-      # VM de desarrollo
-      nixos-vm = mkHost {
-        hostname = "vm";
-        monitorSettings = {
-          name = "Virtual-1";
-          width = 1920;
-          height = 1080;
-          refreshRate = 60;
-          scale = 1.0;
-        };
-      };
-
-      # Thinkpad - trabajo y programación
-      thinkpad = mkHost {
-        hostname = "thinkpad";
-        monitorSettings = {
-          name = "eDP-1";  # Cambiar según tu monitor
-          width = 1920;
-          height = 1080;
-          refreshRate = 60;
-          scale = 1.0;
-        };
-      };
-
-      # Aorus - gaming y desarrollo ocasional
-      aorus = mkHost {
-        hostname = "aorus";
-        monitorSettings = {
-          name = "eDP-1";  # Cambiar según tu monitor
-          width = 1920;
-          height = 1080;
-          refreshRate = 144;  # 144Hz probablemente
-          scale = 1.0;
-        };
-      };
-
-    };
-  };
 }
