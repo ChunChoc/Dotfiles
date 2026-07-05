d04a3ead0d7cf29b675609bd4bdbd6ab3bd4e3ea
ChunChoc <chunchoc@protonmail.com>
2026-07-04 21:01:03 -0600

    agregué tmux

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -4,14 +4,30 @@
   # Paquetes de usuario
   home.packages = with pkgs; [
     # GUI Utils
-    alacritty nautilus brave mullvad-browser
-    mpv imv zathura file-roller localsend
-    obsidian tauon
+    alacritty
+    nautilus
+    brave
+    mullvad-browser
+    mpv
+    imv
+    zathura
+    file-roller
+    localsend
+    obsidian
+    tauon
 
     # CLI Utils
-    git wget curl tree htop
-    eza bat fzf fd
+    git
+    wget
+    curl
+    tree
+    htop
+    eza
+    bat
+    fzf
+    fd
     pokeget-rs
+    tmux
 
     # System Utils
     xwayland-satellite
