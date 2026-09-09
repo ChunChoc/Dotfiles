1b65c1c89e45fb57f610f84e9fcf952fb88fcc54
ChunChoc <127690040+ChunChoc@users.noreply.github.com>
2026-09-09 10:42:26 -0600

    refactor: ahora los wapapers cambian cada hora

--- modules/home/wallpaper.nix
+++ modules/home/wallpaper.nix
@@ -46,7 +46,7 @@ let
     ${pkgs.jq}/bin/jq '
       .wallpaperTransition = "disc"
       | .includedTransitions = ["disc"]
-      | .wallpaperCyclingInterval = 1800
+      | .wallpaperCyclingInterval = 3600
       | .nightModeEnabled = true
       | .nightModeTemperature = 3700
       | .nightModeHighTemperature = 6500
